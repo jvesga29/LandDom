b26d55c6571bb7ffe10df3cfe60642f3d4ef35e4
jvesga29 <jvesga29@gmail.com>
2022-10-21 17:00:46 -0500

    Add files via upload

---
+++ js/cargar_menu.js
@@ -0,0 +1,14 @@
+$(function insert_navbar(){
+    $("#navbar").load("assets/navbar.html"); 
+    return this;
+  });
+
+$(function insert_footer(){
+    $("#footer").load("assets/footer.html"); 
+    return this;
+  });
+
+$(function insert_carousel(){
+    $("#carousel").load("assets/carousel.html"); 
+    return this;
+  });
